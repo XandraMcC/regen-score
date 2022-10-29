3ca58b1971eb986f7969d6765255110ddd765915
AlexMcCarroll <alexandra@mccarrollhk.com>
2022-10-29 21:48:11 +0100

    adding minting compontent

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import Head from "next/head";
 import "../styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,6 +8,18 @@ const activeChainId = ChainId.Goerli;
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      {/* <Head>
+        <title>thirdweb NFT Drop Minting Customizable Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Learn How To Use Thirdweb's NFT Drop contract and create a customizable NFT Drop minting page"
+        />
+        <meta
+          name="keywords"
+          content="Thirdweb, thirdweb NFT drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
+        />
+      </Head> */}
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
