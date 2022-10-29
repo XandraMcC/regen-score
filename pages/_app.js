d5c9fc7f8048bc92a74d1aa93f0b214f201ae743
AlexMcCarroll <alexandra@mccarrollhk.com>
2022-10-29 13:26:49 +0100

    added NFT rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import "../styles/globals.css";
 
-// This is the chainId your dApp will work on.
-// const activeChainId = ChainId.Mainnet;
 const activeChainId = ChainId.Goerli;
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,26 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-import { useContract, useContractRead } from "@thirdweb-dev/react";
+import { useContract, useNFT, ThirdwebNftMedia } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const { contract } = useContract("0x6C13d42a2AEa14B86F5Ffc15FD398Ddfe362150C");
-  const { data: name, isLoading } = useContractRead(contract, "name");
+  const { data: nft, isLoading } = useNFT(contract, 0);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
-        </p>
 
         <div className={styles.connect}>
           <ConnectWallet />
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+        <div>
+          {!isLoading && nft ? (
+            <ThirdwebNftMedia metadata={nft.metadata} />
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
+
       </main>
     </div>
   );
